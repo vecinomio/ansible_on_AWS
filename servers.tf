1a80747e4986bf00394e81588c88d8220c17da30
Igor Makienko <imaki@softserveinc.com>
2019-04-11 13:06:05 +0300

    Created simple structure with one master and two clients

---
+++ servers.tf
@@ -0,0 +1,57 @@
+resource "aws_instance" "ansible" {
+  ami             = "${var.ami}" #Amazon Linux
+  instance_type   = "t2.micro"
+  key_name        = "${var.key_name}"
+  monitoring      = "false"
+  #security_groups = ["${aws_security_group.new_Alex_task1_SSH_HTTP_HTTPS_8080.name}"]
+  vpc_security_group_ids = ["sg-0bab7953543f29ecc"] # My-firewall-HTTP-HTTPS-SSH
+  root_block_device = {
+    volume_size = "8"
+    volume_type = "gp2"
+  }
+
+  tags {
+    Name = "Ansible-master"
+  }
+  provisioner "file" {
+    source      = "./frankfurt_key1.pem"
+    destination = "~/.ssh/frankfurt_key1.pem"
+  }
+  provisioner "file" {
+    source      = "./frankfurt_key2.pem"
+    destination = "~/.ssh/frankfurt_key2.pem"
+  }
+  provisioner "file" {
+    source      = "./ans_prj"
+    destination = "~/ans_prj"
+  }
+  provisioner "remote-exec" {
+    inline = [
+      "sudo pip install ansible",
+      "sudo chmod 400 ~/.ssh/frankfurt_key*"
+    ]
+  }
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = "${file("imaki_Frankfurt.pem")}"
+  }
+}
+
+resource "aws_instance" "client" {
+  count = 2
+  ami             = "${var.ami}" #Amazon Linux
+  instance_type   = "t2.micro"
+  key_name        = "frankfurt_key${count.index + 1}"
+  monitoring      = "false"
+  #security_groups = ["${aws_security_group.new_Alex_task1_SSH_HTTP_HTTPS_8080.name}"]
+  vpc_security_group_ids = ["sg-0c4d0b9d26d81d4fc", "sg-0bab7953543f29ecc"]
+  root_block_device = {
+    volume_size = "8"
+    volume_type = "gp2"
+  }
+
+  tags {
+    Name = "Client-${count.index + 1}"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+# Variables
+
+variable "ami" {
+    default = "ami-0cfbf4f6db41068ac"
+}
+
+variable "key_name" {
+    default = "imaki_Frankfurt"
+}
